c4f731f4fd4783e99cbf5f62b6071913ed6ebcee
soumik.dutta <soumik.dutta@sprinklr.com>
2024-05-31 15:43:46 +0530

    Add dataloader support

--- product/src/main/java/com/chegg/federation/product/query/ProductQuery.java
+++ product/src/main/java/com/chegg/federation/product/query/ProductQuery.java
@@ -1,12 +1,18 @@
 package com.chegg.federation.product.query;
 
 import com.chegg.federation.product.model.Product;
+import graphql.kickstart.execution.context.GraphQLContext;
 import io.leangen.graphql.annotations.GraphQLArgument;
+import io.leangen.graphql.annotations.GraphQLEnvironment;
 import io.leangen.graphql.annotations.GraphQLQuery;
+import io.leangen.graphql.execution.ResolutionEnvironment;
+import org.dataloader.DataLoader;
+import org.dataloader.DataLoaderRegistry;
 import org.springframework.stereotype.Component;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
 
 @Component
 public class ProductQuery {
@@ -20,6 +26,16 @@ public class ProductQuery {
         Product product2 = new Product("2","Couch", 500);
         mockResult.add(product2);
         return mockResult;
+    }
+
+    @GraphQLQuery(name = "getProductByID")
+    public CompletableFuture<Product> getProduct(@GraphQLArgument(name="id") String upc, @GraphQLEnvironment ResolutionEnvironment env) {
+        DataLoaderRegistry registry = ((GraphQLContext) env.dataFetchingEnvironment.getContext()).getDataLoaderRegistry();
+        DataLoader<String, Product> productLoader = registry.getDataLoader("productDataLoader");
+        if (productLoader != null) {
+            return productLoader.load(upc);
+        }
+        throw new IllegalStateException("No product data loader found");
     }
 
 
