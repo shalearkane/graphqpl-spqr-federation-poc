0c050974640bce801fe827a6fd4e6358c9d97373
soumik.dutta <soumik.dutta@sprinklr.com>
2024-06-12 14:27:41 +0530

    fix stuff

--- apollo-federation-js/cmd.sh
+++ apollo-federation-js/cmd.sh
@@ -1,1 +1,5 @@
+rover supergraph compose --output ./supergraph.graphql --config ./supergraph-config.yaml
+./router --supergraph supergraph.graphql
+
+
 docker build --tag router -f apollo-router.dockerfile . && docker run -p 4000:4000 router
